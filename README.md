62c6528e77e305f04f260eed2c6adef1739e23b8
Jake Redekopp <tjredekopp@google.com>
2019-09-03 19:24:44 +0000

    First useful version.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# ultimate
+# Ultimate
+
+## CUC Team History Finder
+cucteams.py scrapes the Ultimate Canada website for teams/players that have participated in CUC and determines which teams have most in common with each other.
+
+To run, ensure you have Beautiful Soup 4 installed (pip3 install beatifulsoup4), and run: `python3 cucteams.py`.
+
+For the first run, you will need to pass an additional argument - your login cookie from the CUC website. To get this, log in to canadianultimate.com, then grab the tsid cookie value (in Chrome, go to chrome://settings/cookies/detail?site=canadianultimate.com and grab the "Content" for the tsid cookie).
+
+Subsequent runs do not require the cookie because the pages are cached.
--- cucteams.py
+++ cucteams.py
@@ -3,11 +3,13 @@
 # requires bs4: pip3 install beautifulsoup4
 
 import collections
+import operator
 import os
 import requests
 import sys
 from bs4 import BeautifulSoup
 
+Team = collections.namedtuple('Team', ['name', 'url', 'year'])
 
 all_players = collections.defaultdict(list)
 all_teams = collections.defaultdict(list)
@@ -15,59 +17,108 @@ all_teams = collections.defaultdict(list)
 
 def process_team(team_div, year):
   team_name = team_div.find_all('h3')[0].get_text()
-  team_w_year = '%s (%s)' % (team_name, year)
-  print(team_name)
+  team_url_l = team_div.find_all('a', attrs={'class': 'media-item-tile media-item-tile-normal media-item-tile-cover'})
+  team_url = team_url_l[0].get('href')[9:]
+  team = Team(team_name, team_url, year)
 
   clusters = team_div.find_all(attrs={'class': 'gender-cluster'})
   for cluster in clusters:
     gender = 'female'
     if cluster.find_all('h5')[0].get_text().startswith('Male'):
       gender = 'male'
-    print('gender:', gender)
     players = cluster.find_all('a')
     for player in players:
       player_name = player.get('href')[9:]
-      print(player_name)
-      all_players[player_name].append(team_w_year)
-      all_teams[team_w_year].append(player_name)
+      all_players[player_name].append(team)
+      all_teams[team].append(player_name)
 
 
-contents = ''
+def find_past_teams(team, players):
+  other_team_counts = collections.defaultdict(int)
+  other_franchise_counts = collections.defaultdict(int)
+  other_franchise_players = collections.defaultdict(list)
+  for player_name in players:
+    franchises_covered = [team.name]
+    for other_team in all_players[player_name]:
+      if other_team == team:
+        continue
+      other_team_counts['%s (%s)' % (other_team.name, other_team.year)] += 1
+      if other_team.name in franchises_covered:
+        continue
+      franchises_covered.append(other_team.name)
+      other_franchise_counts[other_team.name] += 1
+      other_franchise_players[other_team.name].append(player_name)
+
+  #sorted_other_teams = sorted(
+  #    other_team_counts.items(), key=operator.itemgetter(1), reverse=True)
+  #print("%s top 5 teams: %s" % (team.name, sorted_other_teams[0:5]))
+
+  sorted_other_franchises = sorted(
+      other_franchise_counts.items(), key=operator.itemgetter(1), reverse=True)
+  top_other_franchise_players = []
+  for franchise_name, count in sorted_other_franchises:
+    top_other_franchise_players.append(
+        (franchise_name, count, other_franchise_players[franchise_name]))
+  print("%s top 5 franchises: %s" % (team.name, top_other_franchise_players[0:5]))
+
 cachedir = '/tmp/cuccache'
-if not os.path.exists(cachefile):
+if not os.path.exists(cachedir):
   os.mkdir(cachedir)
 if not os.path.isdir(cachedir):
   exit('Cache dir error.')
 
-cachefile = '/tmp/cuccache'
-if os.path.exists(cachefile):
-  with open(cachefile) as f:
-    contents = f.read()
-
+#TODO: URLs for other years / tournaments.
+url_prefix = 'https://canadianultimate.com/en_ca/e/'
 urls = [
-(2019, 'https://canadianultimate.com/en_ca/e/2019-cuc-adult-series/teams')
+('CUC2019', 2019, url_prefix + '2019-cuc-adult-series/teams'),
+('CUC2017', 2017, url_prefix + 'cuc-series/teams'),
+('CUCM2016', 2016, url_prefix + 'cuc-mixed-2016/teams'),
+('CUC2015', 2015, url_prefix + '2015-canadian-ultimate-championships/teams')
+# TODO: For 2014, rosters are not listed on the main page. Need to go to:
+# https://canadianultimate.com/en_ca/t/TEAMNAME/roster?division=4316
+# for each team and parse those pages.
+#('CUC2014', 2014, url_prefix + '2014-cuc-seriesadult/teams')
 ]
-for year, url in urls:
-  print(year, url)
+for event, year, url in urls:
+  print(event, year, url)
 
-  if len(contents) < 5:
-    if len(sys.argv) < 2:
-      sys.exit('Required argument: tsid cookie token')
+  pagenum = 1
+  while pagenum < 20:
+    contents = ''
+    cachefile = os.path.join(cachedir, '%s-%02d' % (event, pagenum))
+    if os.path.exists(cachefile):
+      with open(cachefile) as f:
+        contents = f.read()
 
-  #TODO: URLs for other years / tournaments.
-  #TODO: ?page=N until it fails
-  cookies = dict(tsid=sys.argv[1])
-  response = requests.get(url, cookies=cookies)
-  print(response)
-  contents = response.text
-  with open(cachefile, 'w') as f:
-    f.write(contents)
+    if len(contents) == 0:
+      if len(sys.argv) < 2:
+        sys.exit('Required argument: tsid cookie token')
 
-  soup = BeautifulSoup(contents, "html.parser")
+      cookies = dict(tsid=sys.argv[1])
+      requrl = url
+      if pagenum > 1:
+        requrl += '?page=%d' % pagenum
+      response = requests.get(requrl, cookies=cookies)
+      if response.status_code != 200:
+        sys.exit('Error: %s' % response)
+      contents = response.text
+      with open(cachefile, 'w') as f:
+        f.write(contents)
 
-  TEAM_DIV_CLASS = 'span4 media-item-wrapper spacer1'
-  team_divs = soup.find_all(attrs={'class': TEAM_DIV_CLASS})
-  for div in team_divs:
-    process_team(div, year)
+    soup = BeautifulSoup(contents, "html.parser")
 
-  print(all_players)
+    TEAM_DIV_CLASS = 'span4 media-item-wrapper spacer1'
+    team_divs = soup.find_all(attrs={'class': TEAM_DIV_CLASS})
+    if len(team_divs) == 0:
+      if pagenum == 1:
+        exit('No teams found on page %s.' % pagenum)
+      print('No more teams on page %d for url "%s".' % (pagenum, url))
+      break
+    for div in team_divs:
+      process_team(div, year)
+
+    pagenum += 1
+
+for team, players in all_teams.items():
+  find_past_teams(team, players)
+
