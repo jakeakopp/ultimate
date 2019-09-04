b13be21d9e1ad1dcdf55d4d6aa215f4edda4a3e0
Jake Redekopp <tjredekopp@google.com>
2019-09-04 14:05:34 +0000

    Change to use "franchises" that span years instead of just teams.

--- cucteams.py
+++ cucteams.py
@@ -1,24 +1,88 @@
 #!/usr/bin/env python3
 
-# requires bs4: pip3 install beautifulsoup4
-
 import collections
 import operator
 import os
+import re
 import requests
 import sys
 from bs4 import BeautifulSoup
 
+Franchise = collections.namedtuple('Franchise', ['name', 'url'])
 Team = collections.namedtuple('Team', ['name', 'url', 'year'])
 
+FRANCHISE_URL_TEMPLATE = (
+    'https://canadianultimate.com/en_ca/t/%s/roster?division=')
+
+CACHEDIR = '/tmp/cuccache'
+if not os.path.exists(CACHEDIR):
+  os.mkdir(CACHEDIR)
+if not os.path.isdir(CACHEDIR):
+  exit('Cache dir error.')
+
 all_players = collections.defaultdict(list)
+players_to_franchises = collections.defaultdict(list)
 all_teams = collections.defaultdict(list)
+all_franchises = collections.defaultdict(list)
+
+def download_page(url, cachefilename):
+  cachefile = os.path.join(CACHEDIR, cachefilename)
+  if os.path.exists(cachefile):
+    with open(cachefile) as f:
+      contents = f.read()
+      return contents
+
+  if len(sys.argv) < 2:
+      sys.exit('Required argument: tsid cookie token')
+
+  print('downloading %s' % url)
+
+  cookies = dict(tsid=sys.argv[1])
+  response = requests.get(url, cookies=cookies)
+  if response.status_code != 200:
+    sys.exit('Error fetching "%s": %s' % (url, response))
+  contents = response.text
+  with open(cachefile, 'w') as f:
+    f.write(contents)
+
+  return contents
+
+
+PLAYER_LINK_CLASS = (
+    'media-item-tile media-item-tile-small media-item-tile-cover')
+
+
+# There are often multiple "different" teams which are the same team in
+# different years. e.g. union, union-1, ..., union-8 are all the same.
+# However, union-9 and union-10 are actually different.
+# TODO: Download all "-%d"-suffixed versions of the team and assume they
+# are the same team. If they truly are a different team, there shouldn't
+# be much if any overlap so the results won't be affected.
+def load_players_for_franchise(franchise):
+  if len(all_franchises[franchise]) > 0:
+    return
+
+  base_url = FRANCHISE_URL_TEMPLATE % franchise.url
+  base_page = download_page(base_url, '%s-roster' % franchise.url)
+
+  soup = BeautifulSoup(base_page, "html.parser")
+  rows = soup.find_all('div', attrs={'class': 'row-fluid'})
+  for row in rows:
+    for player_soup in row.find_all('a', attrs={'class': PLAYER_LINK_CLASS}):
+      player_name = player_soup.get('href')[9:]
+      players_to_franchises[player_name].append(franchise)
+
+  all_franchises[franchise] = 'processed'
 
 
 def process_team(team_div, year):
   team_name = team_div.find_all('h3')[0].get_text()
-  team_url_l = team_div.find_all('a', attrs={'class': 'media-item-tile media-item-tile-normal media-item-tile-cover'})
-  team_url = team_url_l[0].get('href')[9:]
+
+  player_link_class = (
+      'media-item-tile media-item-tile-normal media-item-tile-cover')
+  team_url_l = team_div.find_all('a', attrs={'class': player_link_class})
+  full_team_url = team_url_l[0].get('href')
+  team_url = full_team_url[full_team_url.rfind('/')+1:]
   team = Team(team_name, team_url, year)
 
   clusters = team_div.find_all(attrs={'class': 'gender-cluster'})
@@ -32,26 +96,29 @@ def process_team(team_div, year):
       all_players[player_name].append(team)
       all_teams[team].append(player_name)
 
+  load_players_for_franchise(Franchise(team_name, team_url))
+
 
 def find_past_teams(team, players):
   other_team_counts = collections.defaultdict(int)
   other_franchise_counts = collections.defaultdict(int)
   other_franchise_players = collections.defaultdict(list)
   for player_name in players:
-    franchises_covered = [team.name]
     for other_team in all_players[player_name]:
       if other_team == team:
         continue
       other_team_counts['%s (%s)' % (other_team.name, other_team.year)] += 1
-      if other_team.name in franchises_covered:
-        continue
-      franchises_covered.append(other_team.name)
-      other_franchise_counts[other_team.name] += 1
-      other_franchise_players[other_team.name].append(player_name)
+    #sorted_other_teams = sorted(
+    #    other_team_counts.items(), key=operator.itemgetter(1), reverse=True)
+    #print("%s top 5 teams: %s" % (team.name, sorted_other_teams[0:5]))
 
-  #sorted_other_teams = sorted(
-  #    other_team_counts.items(), key=operator.itemgetter(1), reverse=True)
-  #print("%s top 5 teams: %s" % (team.name, sorted_other_teams[0:5]))
+    franchises_covered = [team.name]#url]
+    for other_franchise in players_to_franchises[player_name]:
+      if other_franchise.name in franchises_covered:
+        continue
+      franchises_covered.append(other_franchise.name)
+      other_franchise_counts[other_franchise.name] += 1
+      other_franchise_players[other_franchise.name].append(player_name)
 
   sorted_other_franchises = sorted(
       other_franchise_counts.items(), key=operator.itemgetter(1), reverse=True)
@@ -59,13 +126,8 @@ def find_past_teams(team, players):
   for franchise_name, count in sorted_other_franchises:
     top_other_franchise_players.append(
         (franchise_name, count, other_franchise_players[franchise_name]))
-  print("%s (%s) top 5 franchises: %s" % (team.name, team.year, top_other_franchise_players[0:5]))
+  print("\n%s (%s) top 5 franchises: %s" % (team.name, team.year, top_other_franchise_players[0:5]))
 
-cachedir = '/tmp/cuccache'
-if not os.path.exists(cachedir):
-  os.mkdir(cachedir)
-if not os.path.isdir(cachedir):
-  exit('Cache dir error.')
 
 #TODO: URLs for other tournaments? University? 4s?
 url_prefix = 'https://canadianultimate.com/en_ca/e/'
@@ -87,7 +149,7 @@ for event, year, url in urls:
   pagenum = 1
   while pagenum < 20:
     contents = ''
-    cachefile = os.path.join(cachedir, '%s-%02d' % (event, pagenum))
+    cachefile = os.path.join(CACHEDIR, '%s-%02d' % (event, pagenum))
     if os.path.exists(cachefile):
       with open(cachefile) as f:
         contents = f.read()
