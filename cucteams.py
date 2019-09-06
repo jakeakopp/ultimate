457e1bf92b05dabe3ab81e49e5d059cece1d8aef
Jake Redekopp <tjredekopp@google.com>
2019-09-06 13:18:45 +0000

    Download all instances of a team (team, team-1, team-2, etc.).

--- cucteams.py
+++ cucteams.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import argparse
 import collections
 import operator
 import os
@@ -25,22 +26,30 @@ players_to_franchises = collections.defaultdict(list)
 all_teams = collections.defaultdict(list)
 all_franchises = collections.defaultdict(list)
 
-def download_page(url, cachefilename):
+
+def download_page(url, cachefilename, args):
   cachefile = os.path.join(CACHEDIR, cachefilename)
   if os.path.exists(cachefile):
     with open(cachefile) as f:
       contents = f.read()
+      if contents == '404':
+        raise Error404
       return contents
 
-  if len(sys.argv) < 2:
-      sys.exit('Required argument: tsid cookie token')
+  if args.tsid_cookie == '':
+      sys.exit('Specify --tsid_cookie to download pages.')
 
   print('downloading %s' % url)
 
-  cookies = dict(tsid=sys.argv[1])
+  cookies = dict(tsid=args.tsid_cookie)
   response = requests.get(url, cookies=cookies)
-  if response.status_code != 200:
-    sys.exit('Error fetching "%s": %s' % (url, response))
+  if response.status_code == 404:
+    with open(cachefile, 'w') as f:
+      f.write('404')
+    raise Error404
+  elif response.status_code != 200:
+    raise Exception('url: %s, response: %s' % (url, response))
+
   contents = response.text
   with open(cachefile, 'w') as f:
     f.write(contents)
@@ -51,31 +60,53 @@ def download_page(url, cachefilename):
 PLAYER_LINK_CLASS = (
     'media-item-tile media-item-tile-small media-item-tile-cover')
 
+def create_franchise(name, url):
+  # Match anything ending in a dash and one or two digits.
+  m = re.match('(.*)-\d\d?$', url)
+  if m is None:
+    return Franchise(name, url)
+  return Franchise(name, m.group(1))
+
+
 
 # There are often multiple "different" teams which are the same team in
 # different years. e.g. union, union-1, ..., union-8 are all the same.
 # However, union-9 and union-10 are actually different.
-# TODO: Download all "-%d"-suffixed versions of the team and assume they
+# So we download all "-%d"-suffixed versions of the team and assume they
 # are the same team. If they truly are a different team, there shouldn't
 # be much if any overlap so the results won't be affected.
-def load_players_for_franchise(franchise):
+def load_players_for_franchise(franchise, args):
   if len(all_franchises[franchise]) > 0:
     return
 
-  base_url = FRANCHISE_URL_TEMPLATE % franchise.url
-  base_page = download_page(base_url, '%s-roster' % franchise.url)
+  i = 0
+  # Upper bound of 30 to avoid spamming the site by accident.
+  # Note that there are 28 different registrations for "Phoenix".
+  while i < 30:
+    url_name = franchise.url
+    if i > 0:
+      url_name += '-%d' % i
+    base_url = FRANCHISE_URL_TEMPLATE % url_name
+    base_page = None
+    try:
+      base_page = download_page(base_url, '%s-roster' % url_name, args)
+    except Error404:
+      #print('Didn\'t find more rosters at', url_name)
+      break
 
-  soup = BeautifulSoup(base_page, "html.parser")
-  rows = soup.find_all('div', attrs={'class': 'row-fluid'})
-  for row in rows:
-    for player_soup in row.find_all('a', attrs={'class': PLAYER_LINK_CLASS}):
-      player_name = player_soup.get('href')[9:]
-      players_to_franchises[player_name].append(franchise)
+    soup = BeautifulSoup(base_page, "html.parser")
+    rows = soup.find_all('div', attrs={'class': 'row-fluid'})
+    for row in rows:
+      for player_soup in row.find_all('a', attrs={'class': PLAYER_LINK_CLASS}):
+        player_name = player_soup.get('href')[9:]
+        players_to_franchises[player_name].append(franchise)
+
+    i += 1
 
   all_franchises[franchise] = 'processed'
 
 
-def process_team(team_div, year):
+def process_team(team_div, year, args):
   team_name = team_div.find_all('h3')[0].get_text()
 
   player_link_class = (
@@ -96,7 +127,7 @@ def process_team(team_div, year):
       all_players[player_name].append(team)
       all_teams[team].append(player_name)
 
-  load_players_for_franchise(Franchise(team_name, team_url))
+  load_players_for_franchise(create_franchise(team_name, team_url), args)
 
 
 def find_past_teams(team, players):
@@ -126,7 +157,23 @@ def find_past_teams(team, players):
   for franchise_name, count in sorted_other_franchises:
     top_other_franchise_players.append(
         (franchise_name, count, other_franchise_players[franchise_name]))
-  print("\n%s (%s) top 5 franchises: %s" % (team.name, team.year, top_other_franchise_players[0:5]))
+  print("\n%s (%s) top 5 franchises: %s" %
+        (team.name, team.year, top_other_franchise_players[0:5]))
+
+
+class Error404(Exception):
+  pass
+
+
+def _parse_args():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--years', type=str, default='2019',
+                      help='Comma-separated years to print reports for.')
+  parser.add_argument(
+      '--tsid_cookie', type=str, default='',
+      help='tsid cookie value to use to access canadianultimate.com.')
+
+  return parser.parse_args(sys.argv[1:])
 
 
 #TODO: URLs for other tournaments? University? 4s?
@@ -137,53 +184,47 @@ urls = [
 ('CUC2017', 2017, url_prefix + 'cuc-series/teams'),
 ('CUCM2016', 2016, url_prefix + 'cuc-mixed-2016/teams'),
 ('CUC2016', 2016, url_prefix + 'cuc-2016/teams'),
-('CUC2015', 2015, url_prefix + '2015-canadian-ultimate-championships/teams')
+('CUC2015', 2015, url_prefix + '2015-canadian-ultimate-championships/teams'),
 # TODO: For 2014, rosters are not listed on the main page. Need to go to:
 # https://canadianultimate.com/en_ca/t/TEAMNAME/roster?division=4316
 # for each team and parse those pages.
-#('CUC2014', 2014, url_prefix + '2014-cuc-seriesadult/teams')
+# However, scraping this URL still causes the rosters to be populated for any
+# "franchises" that didn't participate in any of the other events.
+('CUC2014', 2014, url_prefix + '2014-cuc-seriesadult/teams')
 ]
-for event, year, url in urls:
-  print(event, year, url)
+def main():
+  args = _parse_args()
 
-  pagenum = 1
-  while pagenum < 20:
-    contents = ''
-    cachefile = os.path.join(CACHEDIR, '%s-%02d' % (event, pagenum))
-    if os.path.exists(cachefile):
-      with open(cachefile) as f:
-        contents = f.read()
-
-    if len(contents) == 0:
-      if len(sys.argv) < 2:
-        sys.exit('Required argument: tsid cookie token')
-
-      cookies = dict(tsid=sys.argv[1])
+  for event, year, url in urls:
+    pagenum = 1
+    # Upper bound of 20 to avoid spamming the site by accident.
+    while pagenum < 20:
       requrl = url
-      print('downloading page %s' % pagenum)
       if pagenum > 1:
         requrl += '?page=%d' % pagenum
-      response = requests.get(requrl, cookies=cookies)
-      if response.status_code != 200:
-        sys.exit('Error: %s' % response)
-      contents = response.text
-      with open(cachefile, 'w') as f:
-        f.write(contents)
+      print(event, year, requrl)
+      contents = download_page(requrl, '%s-%02d' % (event, pagenum), args)
 
-    soup = BeautifulSoup(contents, "html.parser")
+      soup = BeautifulSoup(contents, "html.parser")
 
-    TEAM_DIV_CLASS = 'span4 media-item-wrapper spacer1'
-    team_divs = soup.find_all(attrs={'class': TEAM_DIV_CLASS})
-    if len(team_divs) == 0:
-      if pagenum == 1:
-        exit('No teams found on page %s.' % pagenum)
-      print('No more teams on page %d for url "%s".' % (pagenum, url))
-      break
-    for div in team_divs:
-      process_team(div, year)
+      TEAM_DIV_CLASS = 'span4 media-item-wrapper spacer1'
+      team_divs = soup.find_all(attrs={'class': TEAM_DIV_CLASS})
+      if len(team_divs) == 0:
+        if pagenum == 1:
+          exit('No teams found on page %s.' % pagenum)
+        print('No more teams on page %d for url "%s".' % (pagenum, url))
+        break
+      for div in team_divs:
+        process_team(div, year, args)
 
-    pagenum += 1
+      pagenum += 1
 
-for team, players in all_teams.items():
-  find_past_teams(team, players)
+  for team, players in all_teams.items():
+    if str(team.year) not in args.years.split(','):
+      continue
+    find_past_teams(team, players)
+
+
+if __name__ == '__main__':
+  main()
 
